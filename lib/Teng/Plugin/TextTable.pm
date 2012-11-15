382ce569f6f80edc9301450fcd9dda6c82e83cf3
tokuhirom <tokuhirom@gmail.com>
2012-11-15 19:26:25 +0900

    implemented.

--- lib/Teng/Plugin/TextTable.pm
+++ lib/Teng/Plugin/TextTable.pm
@@ -3,7 +3,36 @@ use strict;
 use warnings;
 use 5.010001;
 our $VERSION = '0.01';
+use Text::SimpleTable;
+use Carp ();
+use List::Util ();
 
+our @EXPORT = qw/draw_text_table/;
+
+sub draw_text_table {
+    my ($self, $table_name, $where, $opts, $cols) = @_;
+    my $table = $self->schema->get_table($table_name)
+        or Carp::croak("Unknown table: $table_name");
+    unless ($cols) {
+        $cols = $table->columns();
+    }
+
+    my $iter = $self->search($table_name, $where, $opts);
+    $iter->suppress_object_creation(1);
+    my @rows = $iter->all;
+
+    my @headers = map { [length $_, $_] } @$cols;
+    for my $i (0..@$cols-1) {
+        for my $row (@rows) {
+            $headers[$i]->[0] = List::Util::max($headers[$i]->[0], length($row->{$cols->[$i]}));
+        }
+    }
+    my $tt = Text::SimpleTable->new(@headers);
+    for my $row (@rows) {
+        $tt->row(map { $row->{$_} } @$cols);
+    }
+    return $tt->draw;
+}
 
 
 1;
@@ -13,15 +42,20 @@ __END__
 
 =head1 NAME
 
-Teng::Plugin::TextTable - ...
+Teng::Plugin::TextTable - Make text table from database.
 
 =head1 SYNOPSIS
 
-  use Teng::Plugin::TextTable;
+    package My::DB;
+    __PACKAGE__->load_plugin('TextTable');
+
+    package main;
+    my $db = My::DB->new(...);
+    print $db->draw_text_table('user', {id => { '>', 50 }});
 
 =head1 DESCRIPTION
 
-Teng::Plugin::TextTable is
+Teng::Plugin::TextTable is text table renderer plugin for L<Teng>.
 
 =head1 AUTHOR
 
